8c4c6b60e12b355eb777526615bbc27d5b90f9c9
Pákozdi Károly <h668139@stud.u-szeged.hu>
2018-12-03 02:03:58 +0100

    Implementing title based search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
 
   def search
-    query = params[:q]
-    if query && query.size >= 3
-      @result_set = Book.where('title LIKE ?', '%' + query + '%').all
+    @query = params[:q]
+    if @query && @query.size >= 3
+      @result_set = Book.where('unaccent(lower(title)) LIKE unaccent(lower(?))', '%' + @query + '%').all
     else
       redirect_to root_url, :notice => 'No search query was specified.'
     end
